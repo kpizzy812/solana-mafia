08f50c0b6a909be90fce6db1c6ed502f7674615d
kpizzy812 <officialqwe@gmail.com>
2025-07-28 23:54:54 +0300

    changes

--- programs/solana-mafia/src/lib.rs
+++ programs/solana-mafia/src/lib.rs
@@ -1,18 +1,23 @@
-// Импортируем handlers из instructions (ТОЛЬКО initialize)
+use anchor_lang::prelude::*;
+
+// Нужны для initialize.rs!
+pub mod constants;
+pub mod error;      
+pub mod instructions; 
+pub mod state;
+pub mod utils;
+
 use instructions::{
     initialize_handler,
-};
-
-// Импортируем контексты из instructions (ТОЛЬКО initialize)
-use instructions::{
     Initialize,
 };
+
+declare_id!("Hnyyopg1fsQGY1JqEsp8CPZk1KjDKsAoosBJJi5ZpegU");
 
 #[program]
 pub mod solana_mafia {
     use super::*;
 
-    /// Initialize the game with treasury wallet
     pub fn initialize(ctx: Context<Initialize>, treasury_wallet: Pubkey) -> Result<()> {
         initialize_handler(ctx, treasury_wallet)
     }
